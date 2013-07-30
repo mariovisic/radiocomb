749e70ac2128ea7b91022e4c0868004230db3d5f
Mario Visic <mario@mariovisic.com>
2013-07-30 21:36:35 +1000

    Remove some non required steps from the build pipeline

--- config.rb
+++ config.rb
@@ -16,10 +16,7 @@ set :relative_links, true
 
 # Build-specific configuration
 configure :build do
-  activate :minify_css
-  activate :minify_javascript
   activate :minify_html
-  activate :asset_hash
   activate :gzip
   activate :relative_assets
 
